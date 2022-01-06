4b0a9fb07b52b3517a9d73d2ab689624d18ed9ef
jacobdeutsch <46475670+jacobdeutsch@users.noreply.github.com>
2022-01-05 20:06:54 -0500

    Update 1.py
    Day 1 completed.

--- 2015/1.py
+++ 2015/1.py
@@ -1,1 +1,26 @@
+# Day 1 Part 1
+import pyperclip # just going to get the clue from the clipboard
 
+clue = pyperclip.paste() # pasting clue into clue variable.
+
+floor = 0 # set floor to 0 to start
+for i in range(0, len(clue)): # iterate through each character of the clue, adding 1 to floor for each ( and subtracting for every )
+    if clue[i] == '(':
+        floor += 1
+    elif clue[i] == ')':
+        floor -= 1
+
+print('Santa ends up on floor ' + str(floor) + '.')  # print out the answer
+
+
+# Day 1 Part 2
+floor = 0 # reset floor to 0 or else you end up with the wrong number...
+for i in range(0, len(clue)): # iterate through each character of the clue, adding 1 to floor for each ( and subtracting for every )
+    if clue[i] == '(':
+        floor += 1
+    elif clue[i] == ')':
+        floor -= 1
+    
+    if floor < 0:
+        print('Santa first ends up in the basement at position ' + str(i + 1) + '.')
+        break
